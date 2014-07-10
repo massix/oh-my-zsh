824caefde19fd644e18ec59d9f6b2b80ed97adf9
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2014-07-10 09:59:05 +0200

    Modify bzr_prompt_info

--- lib/bzr.zsh
+++ lib/bzr.zsh
@@ -1,10 +1,19 @@
 ## Bazaar integration
 ## Just works with the GIT integration just add $(bzr_prompt_info) to the PROMPT
 function bzr_prompt_info() {
-	BZR_CB=`bzr nick 2> /dev/null | grep -v "ERROR" | cut -d ":" -f2 | awk -F / '{print "bzr::"$1}'`
-	if [ -n "$BZR_CB" ]; then
-		BZR_DIRTY=""
-		[[ -n `bzr status` ]] && BZR_DIRTY=" %{$fg[red]%} * %{$fg[green]%}"
-		echo "$ZSH_THEME_SCM_PROMPT_PREFIX$BZR_CB$BZR_DIRTY$ZSH_THEME_GIT_PROMPT_SUFFIX"
-	fi
+  while [[ ${PWD} != "/" ]]; do
+    if [[ -d ".bzr" ]]; then
+      BZR_CB=`cat .bzr/branch/branch.conf | grep bound_location | awk -F / '{print $9}'`
+      if [[ "x${BZR_CB}" == "x" ]]; then
+        BZR_CB="unknown branch"
+      fi
+      BZR_CB="bzr::${BZR_CB}"
+      [[ -n `bzr status` ]] && BZR_DIRTY="%{$fg[red]%} ·%{$fg[blue]%}"
+      echo "$ZSH_THEME_GIT_PROMPT_PREFIX$BZR_CB$BZR_DIRTY$ZSH_THEME_GIT_PROMPT_SUFFIX"
+      break;
+    fi
+
+    cd ..
+
+  done
 }
