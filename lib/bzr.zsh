39f0bdfb838cafcd4fd43bb4e31a37de18d371d8
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2014-07-29 10:56:59 +0200

    Modified themes

--- lib/bzr.zsh
+++ lib/bzr.zsh
@@ -1,5 +1,6 @@
 ## Bazaar integration
 ## Just works with the GIT integration just add $(bzr_prompt_info) to the PROMPT
+
 function bzr_prompt_info() {
   while [[ ${PWD} != "/" ]]; do
     if [[ -d ".bzr" ]]; then
@@ -7,9 +8,9 @@ function bzr_prompt_info() {
       if [[ "x${BZR_CB}" == "x" ]]; then
         BZR_CB="unknown branch"
       fi
-      BZR_CB="bzr::${BZR_CB}"
-      [[ -n `bzr status` ]] && BZR_DIRTY="%{$fg[red]%} ·%{$fg[blue]%}"
-      echo "$ZSH_THEME_GIT_PROMPT_PREFIX$BZR_CB$BZR_DIRTY$ZSH_THEME_GIT_PROMPT_SUFFIX"
+      BZR_CB="${BZR_CB}"
+      [[ -n `bzr status` ]] && BZR_DIRTY="%{$fg[red]%} %{$fg[yellow]%}✗%{$fg[blue]%}"
+      echo "$ZSH_THEME_BZR_PROMPT_PREFIX$BZR_CB$BZR_DIRTY$ZSH_THEME_BZR_PROMPT_CLEAN"
       break;
     fi
 
