cda7df2cf514cb6430dc9b30a70fb4a031dfeaad
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2014-10-08 13:47:57 +0200

    Added todo-list todo

--- plugins/todo-list/todo-list.plugin.zsh
+++ plugins/todo-list/todo-list.plugin.zsh
@@ -6,6 +6,7 @@ local tdl_db=${HOME}/Dropbox/todo.bin
 local tdl_personal_db=${HOME}/Dropbox/personal.bin
 local tdl_work_db=${HOME}/Dropbox/work.bin
 local tdl_generic_db=${HOME}/Dropbox/todo.bin
+local tdl_todo_list_db=${HOME}/Dropbox/todo-list.bin
 
 function personal_todo() {
   local action=""
@@ -34,6 +35,16 @@ function generic_todo() {
   $tdl_command $action $@ --tododb $tdl_generic_db
 }
 
-alias ptdl = personal_todo
-alias wtdl = work_todo
-alias tdl = generic_todo
+function todo_todo() {
+  local action=""
+  if [[ $1 == "" ]]; then
+    action="list"
+  fi
+
+  $tdl_command $action $@ --tododb $tdl_todo_list_db
+}
+
+alias ptdl=personal_todo
+alias wtdl=work_todo
+alias tdl=generic_todo
+alias ttdl=todo_todo
